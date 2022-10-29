1d85ed83e6e230c9644b7fdc49d4d432f46158f7
H. Saw <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2022-10-29 20:06:50 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,116 @@
+# audio configuration
+def playSoundEffect(name: str):
+    if name == "beep":
+        music.play_tone(466, music.beat(BeatFraction.WHOLE))
+# directional arrow hall latern, similar to OTIS
+def elevatorDirection(direction: number):
+    global display_direction_arrows_frames, display
+    # going up
+    if direction >= 1:
+        display_direction_arrows_frames = [images.create_image("""
+                . . . . .
+                            . . . . .
+                            . . . . .
+                            . . . . .
+                            . . # . .
+            """),
+            images.create_image("""
+                . . . . .
+                            . . . . .
+                            . . . . .
+                            . . # . .
+                            . # # # .
+            """),
+            images.create_image("""
+                . . . . .
+                            . . . . .
+                            . . # . .
+                            . # # # .
+                            . # # # .
+            """),
+            images.create_image("""
+                . . . . .
+                            . . # . .
+                            . # # # .
+                            . # # # .
+                            . # # # .
+            """),
+            images.create_image("""
+                . . # . .
+                            . # # # .
+                            # # # # #
+                            . # # # .
+                            . # # # .
+            """)]
+    elif direction <= -1:
+      # going down
+        display_direction_arrows_frames = [images.create_image("""
+                . . # . .
+                            . . . . .
+                            . . . . .
+                            . . . . .
+                            . . . . .
+            """),
+            images.create_image("""
+                . # # # .
+                            . . # . .
+                            . . . . .
+                            . . . . .
+                            . . . . .
+            """),
+            images.create_image("""
+                . # # # .
+                            . # # # .
+                            . . # . .
+                            . . . . .
+                            . . . . .
+            """),
+            images.create_image("""
+                . # # # .
+                            . # # # .
+                            . # # # .
+                            . . # . .
+                            . . . . .
+            """),
+            images.create_image("""
+                . # # # .
+                            . # # # .
+                            # # # # #
+                            . # # # .
+                            . . # . .
+            """)]
+    for display_current in display_direction_arrows_frames:
+        display = display_current
+        display.show_image(0)
+        basic.pause(50)
+    while elevator_call_direction:
+        for display_brightness_frame in [False, True]:
+            led.enable(display_brightness_frame)
+            basic.pause(100)
+
+# remotely receive data about the lift
+def on_received_value(name2, value):
+    global elevator_call_direction
+    configuration_floorNumber = 0
+    if name2 == "beep" and value == configuration_floorNumber:
+        playSoundEffect("beep")
+    elif name2 == "dir_" + str(configuration_floorNumber):
+        elevator_call_direction = value
+    else:
+        pass
+radio.on_received_value(on_received_value)
+
+display_direction_arrows_frames: List[Image] = []
+display: Image = None
+elevator_call_direction = 0
+display = images.create_image("""
+    . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,123 @@
+// audio configuration
+function playSoundEffect (name: string) {
+    if (name == "beep") {
+        music.playTone(466, music.beat(BeatFraction.Whole))
+    }
+}
+// directional arrow hall latern, similar to OTIS
+function elevatorDirection (direction: number) {
+    // going up
+    if (direction >= 1) {
+        display_direction_arrows_frames = [
+        images.createImage(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `),
+        images.createImage(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            `),
+        images.createImage(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            . # # # .
+            `),
+        images.createImage(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `),
+        images.createImage(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . # # # .
+            `)
+        ]
+    } else if (direction <= -1) {
+        // going down
+        display_direction_arrows_frames = [
+        images.createImage(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `),
+        images.createImage(`
+            . # # # .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `),
+        images.createImage(`
+            . # # # .
+            . # # # .
+            . . # . .
+            . . . . .
+            . . . . .
+            `),
+        images.createImage(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . . # . .
+            . . . . .
+            `),
+        images.createImage(`
+            . # # # .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        ]
+    }
+    for (let display_current of display_direction_arrows_frames) {
+        display = display_current
+        display.showImage(0)
+        basic.pause(50)
+    }
+    while (elevator_call_direction) {
+        for (let display_brightness_frame of [false, true]) {
+            led.enable(display_brightness_frame)
+            basic.pause(100)
+        }
+    }
+}
+// remotely receive data about the lift
+radio.onReceivedValue(function (name2, value) {
+    let configuration_floorNumber = 0
+    if (name2 == "beep" && value == configuration_floorNumber) {
+        playSoundEffect("beep")
+    } else if (name2 == "dir_" + ("" + configuration_floorNumber)) {
+        elevator_call_direction = value
+    } else {
+    	
+    }
+})
+let elevator_call_direction = 0
+let display_direction_arrows_frames: Image[] = []
+let display: Image = null
+display = images.createImage(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
